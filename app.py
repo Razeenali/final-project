96a5dd65772c0cf9318fe69dc6d956c65c1777ae
Ryan Chen <rchen5243@gmail.com>
2020-07-26 06:45:02 +0000

    update

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def index():
 
 @app.route('/recipes', methods=['POST', 'GET'])
 def recipes():
-    # recipe = model.getRecipeFrom(request.form['foodQ'])
-    # title = model.getTitle(request.form['foodQ'])
-    # prepTime = model.getPrepTime(request.form['foodQ'])
-    # numServings = model.getNumServings(request.form['foodQ'])
-    # print(type(title))
-    info = model.getInfo(request.form['foodQ'])
-    return render_template('recipes.html', info=info)
+    list_dishes = model.getListDishes(request.form['foodQ'])
+    return render_template('recipes.html', list_dishes=list_dishes)
+
+
+@app.route('/ingredients', methods=['POST', 'GET'])
+def ingredients():
+    information = model.getInformation(request.form['dishID'])
+    return render_template('ingredients.html', information=information)
--- model.py
+++ model.py
@@ -1,25 +1,36 @@
 import requests
 import spoonacular as sp
 
-# API key:
-# 4c93056742bb42a8a2536a91e6630495
+# API key: 4c93056742bb42a8a2536a91e6630495
 
 
 def getRecipeFrom(query):
     api = sp.API("4c93056742bb42a8a2536a91e6630495")
-
     num_results = 20
-
-    # Parse an ingredient
     content = requests.get(
-        f"https://api.spoonacular.com/recipes/search?query={query}&number={num_results}" + "&apiKey=" + "4c93056742bb42a8a2536a91e6630495")
+        f"https://api.spoonacular.com/recipes/search?query={query}&number={num_results}&apiKey=4c93056742bb42a8a2536a91e6630495")
     json_response = content.json()
     return json_response
 
 
-def getInfo(query):
+# def getListID(query):
+#     json_response = getRecipeFrom(query)
+#     list_ID = []
+#     for dish in json_response['results']:
+#         list_ID.append(dish['id'])
+#     return list_ID
+
+
+def getListDishes(query):
     json_response = getRecipeFrom(query)
     list_dishes = []
     for dish in json_response['results']:
         list_dishes.append(dish)
     return list_dishes
+
+
+# Get Recipe Information
+def getInformation(query):
+    content = requests.get(f'https://api.spoonacular.com/recipes/{query}/information?apiKey=4c93056742bb42a8a2536a91e6630495&includeNutrition=false')
+    json_response = content.json()
+    return json_response
